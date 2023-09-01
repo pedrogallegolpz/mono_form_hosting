5aa765ec7c7364786e20423dc16559d32bc45822
Pedro Gallego López <55806921+pedrogallegolpz@users.noreply.github.com>
2023-09-01 12:11:46 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Formulario de Registro</title>
+</head>
+<body>
+    <h1>Formulario de Registro</h1>
+    <form id="registroForm">
+        Nombre: <input type="text" id="nombre" required><br>
+        Edad: <input type="number" id="edad" required><br>
+        <input type="submit" value="Registrar">
+    </form>
+
+    <script src="script.js"></script>
+</body>
+</html>
