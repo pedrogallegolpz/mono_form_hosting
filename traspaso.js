93dcc0acd627b033920c8746cce49d45fbe498ed
Pedro Gallego López <pedrogallegolop@gmail.com>
2023-09-01 08:50:26 +0200

    primer commit

---
+++ traspaso.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwibGD3peLlHgB8ayv_K2cVQJ99wY2KMG82voQbaEU_npMIuKegd1u38R21PZM8Lnj1/exec'
+const form = document.forms['submit-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
