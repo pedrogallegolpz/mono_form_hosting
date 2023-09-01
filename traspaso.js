b6fa9917cda4d4db1c86a60c606f580d871fd75a
Pedro Gallego López <55806921+pedrogallegolpz@users.noreply.github.com>
2023-09-01 11:06:21 +0200

    Update traspaso.js

--- traspaso.js
+++ traspaso.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzSTpjf9VpsJuKzH5MIEbiZaavwhI-_UUicwfuuYXA/dev'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwKrnfjHNb8CTbiLYMRznaCTZf6QgHVrWZyLvYH_SHeXiKXHuH4RMFYRhibtnmTsZ4a/exec'
 const form = document.forms['traspasoForm']
 
 form.addEventListener('submit', e => {
