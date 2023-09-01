c0bef8e938d8be921fc57b2c178d9cd3e3f90d37
Pedro Gallego López <55806921+pedrogallegolpz@users.noreply.github.com>
2023-09-01 14:15:23 +0200

    Delete traspaso.js

--- traspaso.js
+++
@@ -1,9 +0,0 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzSTpjf9VpsJuKzH5MIEbiZaavwhI-_UUicwfuuYXA/dev'
-const form = document.forms['traspasoForm']
-
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form), mode: 'no-cors'})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
-})
