83fc66bbabb34ef9e7f82764bc937c2c7e43ff14
Pedro Gallego López <55806921+pedrogallegolpz@users.noreply.github.com>
2023-09-01 12:12:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Formulario de Registro</title>
-</head>
-<body>
-    <h1>Formulario de Registro</h1>
-    <form id="registroForm">
-        Nombre: <input type="text" id="nombre" required><br>
-        Edad: <input type="number" id="edad" required><br>
-        <input type="submit" value="Registrar">
-    </form>
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registroForm");
 
-    <script src="script.js"></script>
-</body>
-</html>
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        // Obtén los valores del formulario.
+        const nombre = document.getElementById("nombre").value;
+        const edad = document.getElementById("edad").value;
+
+        // Crea una línea CSV con los datos del formulario.
+        const csvLine = `${nombre},${edad}`;
+
+        // Llama a la función para agregar la línea al archivo CSV en GitHub.
+        agregarAlCSV(csvLine);
+    });
+});
+
+async function agregarAlCSV(lineaCSV) {
+    // Definir la URL del archivo CSV en tu repositorio.
+    const csvURL = 'https://raw.githubusercontent.com/TU_USUARIO/TU_REPO/main/tu-archivo.csv';
+
+    try {
+        // Obtener el contenido actual del archivo CSV.
+        const respuesta = await fetch(csvURL);
+        const contenidoActual = await respuesta.text();
+
+        // Agregar la nueva línea al contenido.
+        const nuevoContenido = `${contenidoActual}\n${lineaCSV}`;
+
+        // Enviar una solicitud PUT para actualizar el archivo CSV.
+        await fetch(csvURL, {
+            method: 'PUT',
+            body: nuevoContenido,
+        });
+
+        alert('Registro exitoso.');
+
+        // Puedes redirigir al usuario a otra página o realizar cualquier acción adicional aquí.
+    } catch (error) {
+        console.error('Error al agregar al CSV:', error);
+        alert('Error al registrar. Por favor, inténtalo de nuevo.');
+    }
+}
