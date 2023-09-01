50a3c08c78e5dbc763dd0355b68bedad7163ba85
Pedro Gallego López <55806921+pedrogallegolpz@users.noreply.github.com>
2023-09-01 09:30:14 +0200

    Update traspaso.js

--- traspaso.js
+++ traspaso.js
@@ -3,7 +3,7 @@ const form = document.forms['traspasoForm']
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  fetch(scriptURL, { method: 'doPOST', body: new FormData(form)})
     .then(response => console.log('Success!', response))
     .catch(error => console.error('Error!', error.message))
 })
